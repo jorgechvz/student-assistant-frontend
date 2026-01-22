74f53a74d7cddae6718a8beeb534820a1dfc312f
jorgechvz <cha22060@byui.edu>
2026-01-22 10:31:48 -0500

    feat: implement custom authentication hook with login, signup, and logout functionalities

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,60 @@
+import { login, logout, signup } from "@/api/auth.api";
+import { useAuthStore } from "@/context/auth/userStore";
+import type { UserStorage } from "@/context/auth/userStore";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const useAuth = () => {
+  const queryClient = useQueryClient();
+  const { setUser } = useAuthStore();
+
+  const fetchLogin = useMutation({
+    mutationFn: ({ email, password }: { email: string; password: string }) =>
+      login(email, password),
+    onSuccess: (data) => {
+      const userInfo: UserStorage = {
+        id: data.user.id,
+        email: data.user.email,
+        full_name: data.user.full_name,
+        is_active: data.user.is_active,
+      };
+      setUser(userInfo);
+      queryClient.invalidateQueries({ queryKey: ["currentUser"] });
+    },
+  });
+
+  const handleSignUp = useMutation({
+    mutationFn: async ({
+      email,
+      password,
+      full_name,
+    }: {
+      email: string;
+      password: string;
+      full_name: string;
+    }) => {
+      return signup(email, password, full_name);
+    },
+    onSuccess: (data) => {
+      const userInfo: UserStorage = {
+        id: data.user.id,
+        email: data.user.email,
+        full_name: data.user.full_name,
+        is_active: data.user.is_active,
+      };
+      setUser(userInfo);
+      queryClient.invalidateQueries({ queryKey: ["currentUser"] });
+    },
+  });
+
+  const handleLogout = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      setUser(null);
+      queryClient.clear();
+    },
+  });
+
+  return { fetchLogin, handleSignUp, handleLogout };
+};
+
+export default useAuth;
